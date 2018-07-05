eb6f076fdbdb9527130770f9dca19463137542fc
Matthew Huntington Murphy <hutington67@aim.com>
2018-07-05 22:52:08 +0900

    ScriptTestプロジェクト
    ScriptTestとBossスクリプト作ったファイルです。

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour {
+    private int hp = 100;
+    private int power = 25;
+    private int mp = 53;
+
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    public void Defense(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+        this.hp -= damage;
+        Debug.Log(hp);
+    }
+
+    public void Magic()
+    {
+        for (int i = mp; i >= 0; i -= 5)
+        {
+            //this.mp -= magic;
+            if (i >= 5)
+            {
+                Debug.Log("魔法攻撃をした。残りMPは" + i + "。"); 
+            }
+            else
+            {
+                Debug.Log("MPが足りないため魔法が使えない");
+            }
+        }
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        Boss lastboss = new Boss();
+
+        lastboss.Attack();
+        lastboss.Defense(3);
+        lastboss.Magic();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        int[] points = new int[5];
+
+        points[0] = 10;
+        points[1] = 20;
+        points[2] = 30;
+        points[3] = 40;
+        points[4] = 50;
+
+        for (int i = points.Length - 1; i >= 0; i--)
+        {
+           Debug.Log(points[i]); 
+        }
+    }
+}
